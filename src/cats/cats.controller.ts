01d1b7c26392f2a760dea30631df4b1cd2f7a515
famasoon <famasoon@gmail.com>
2021-06-14 23:55:25 +0900

    リダイレクト設定を作成

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Redirect } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
@@ -10,5 +10,13 @@ export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
